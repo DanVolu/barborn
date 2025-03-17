34be6089a4acf862329d9926fed0028ac616ad73
DanVolu <dan.voluj@gmail.com>
2025-03-17 22:18:52 +0200

    Minor responsiveness tweaks.

--- frontend/src/components/pages/sections/Equipment.tsx
+++ frontend/src/components/pages/sections/Equipment.tsx
@@ -1,9 +1,9 @@
 function Equipment() {
   return (
     <>
-      <div className="h-screen bg-gradient-to-br from-[#cfd0d1] via-[#444444] to-[#0d0d0d]">
-        <div className="flex justify-center items-center ">
-          <h2 className="text-4xl font-medium text-[#1E1E1E] py-15">
+      <div className="h-full bg-gradient-to-br from-[#cfd0d1] via-[#444444] to-[#0d0d0d]">
+        <div className="flex justify-start md:justify-center items-center ">
+          <h2 className="text-xl sm:text-xl md:text-2xl lg:text-3xl font-medium text-[#1E1E1E] py-8 sm:py-15 p-6">
             Choose the equipment that’s right for you
           </h2>
         </div>
@@ -14,7 +14,7 @@ function Equipment() {
           <div className="h-[35rem] rounded-md bg-[#1E1E1E]"></div>
         </div>
       </div>
-    </> //cfd0d1 444444 1E1E1E
+    </> //cfd0d1 444444 1E1E1E SCROLLABLE L8R
   );
 }
 
