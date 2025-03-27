553445a2afe28033b4e44cd0eb53f23e7652f963
DanVolu <dan.voluj@gmail.com>
2025-03-27 12:00:19 +0200

    back

--- backend/app/server.ts
+++ backend/app/server.ts
@@ -10,7 +10,7 @@ connectDB();
 const port = process.env.PORT || 7000;
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://barborn-1.onrender.com/"],
+    origin: ["http://localhost:5173", "https://barborn-1.onrender.com"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,13 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000, // Set PORT dynamically
-    host: "0.0.0.0", // Required for Render
-  },
   define: {
     "process.env.BACKEND_URL": JSON.stringify(
       process.env.BACKEND_URL || "http://localhost:7000"
     ),
   },
 });
+// base: "/barborn/",
