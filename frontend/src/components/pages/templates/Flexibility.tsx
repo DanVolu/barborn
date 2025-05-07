abfc373ce2e3bbf28967fa89a73b7616d7fc1a04
DanVolu <dan.voluj@gmail.com>
2025-05-07 14:09:17 +0300

    Minimal changes.

--- frontend/src/components/pages/templates/Flexibility.tsx
+++ frontend/src/components/pages/templates/Flexibility.tsx
@@ -1,23 +1,16 @@
-import { useNavigate } from "react-router-dom";
+import Nav from "../../navbar/Nav"
 
 function Mobility() {
-  const navigate = useNavigate();
   return (
     <main className="flex">
+      <Nav></Nav>
       <div className="h-fit flex flex-col justify-center lg:items-center w-full px-4">
-        <div className="lg:w-6/12 flex items-start justify-start px-8 py-6 text-xl">
-          <a
-            onClick={() => navigate("/")}
-            className="text-[#e0e0e0] cursor-pointer p-1 rounded-lg -ml-4 px-4 bg-[#1a1a1a] shadow-md shadow-black/40 transition-all duration-500 hover:opacity-80"
-          >
-            Back
-          </a>
-        </div>
 
-        <div className="flex flex-col gap-8 justify-between lg:w-6/12 h-full px-4">
+
+        <div className="flex flex-col gap-8 pt-24 justify-between lg:w-6/12 h-full px-4">
           <div className="border-[#444444] p-6 rounded-lg mb-16 bg-gradient-to-br from-[#111111] to-[#111111] shadow-lg shadow-black/40 flex flex-col">
             <h3 className="text-[#e0e0e0] text-3xl md:text-4xl font-semibold mb-4">
-              Introduction to Mobility
+              Introduction to basic mobility
             </h3>
 
             <p className="text-[#e0e0e0] text-lg leading-relaxed">111111111</p>
@@ -66,12 +59,9 @@ function Mobility() {
                 Preferably do them after an upper body workout
               </p>
               <ul className="text-[#e0e0e0] ml-4 list-disc px-4">
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
+                <li>Shoulder dislocates</li>
+                <li>Skin the cat with multiple reps in reserve</li>
+                <li>Butcher's block</li>
               </ul>
             </div>
           </div>
